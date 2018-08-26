e2f7053df43ab27e511535d9d0cd9c9b82cc6e8f
Davi Naizer <davinaizer@gmail.com>
2018-08-26 18:30:45 -0300

    added new config settings and HTML plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,27 @@
-module: {
-  rules: [
-    { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-  ]
-}
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/static/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: "./src/app/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/main.js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" }
+      }
+    ]
+  },
+
+  plugins: [htmlPlugin]
+};

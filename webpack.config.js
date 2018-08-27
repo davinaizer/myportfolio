ba1b447be0203b97eece08dbb4385682b8a4b1cd
Davi Naizer <davinaizer@gmail.com>
2018-08-26 22:41:04 -0300

    Added JSX support and new loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,18 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: { loader: "babel-loader" }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
       }
     ]
   },

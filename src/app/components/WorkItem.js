020f42a53324a4d1ad7ebb471937cde3b2189172
Davi Naizer <davinaizer@gmail.com>
2018-09-07 22:44:21 -0300

    update Work section with new content, component and images

--- src/app/components/WorkItem.js
+++ src/app/components/WorkItem.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import {Row, Col} from 'reactstrap';
+
+import './WorkItem.scss';
 
 export default class WorkItem extends Component {
   constructor(props) {
@@ -6,6 +9,48 @@ export default class WorkItem extends Component {
   }
 
   render() {
-    return (null);
+    const props = this.props;
+    let isFlipped = props.isFlipped;
+
+    return (
+      <Row noGutters className="work-item">
+        <Col md="6">
+          <div className="work-item__thumb">
+            <img src={props.src} alt="" />
+          </div>
+        </Col>
+
+        <Col md="6" className={isFlipped ? 'order-md-first' : ''}>
+          <div
+            className={
+              'work-item__card ' + (isFlipped ? 'work-item__card--flipped' : '')
+            }>
+            <div className="d-flex h-100">
+              <div className="w-100 text-lg-left">
+                <p className="work-item__title">Elit ut sunt id.</p>
+                <p className="work-item__body small">
+                  Sunt dolor commodo minim aliquip id in magna aute id sint
+                  veniam ex dolor cupidatat labore in proident sit in non
+                  occaecat proident id et non enim.
+                </p>
+              </div>
+
+              <div className="work-item__tags">
+                <div className="tag-item">HTML</div>
+                <div className="tag-item">JS</div>
+                <div className="tag-item">SASS</div>
+                <div className="tag-item">BackboneJS</div>
+                <div className="tag-item">React</div>
+                <div className="tag-item">Node.JS</div>
+                <div className="tag-item">Express</div>
+                <div className="tag-item">RequireJS</div>
+                <div className="tag-item">GSAP</div>
+                <div className="tag-item">GULP</div>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    );
   }
 }
--- src/app/containers/Work.js
+++ src/app/containers/Work.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
-import {Container, Row, Col} from 'reactstrap';
+import {Container} from 'reactstrap';
 
+import '../img/work-bg.jpg';
 import '../containers/Work.scss';
 
-// import masterheadImg from '../img/bg-masthead.jpg';
-import demoImg01 from '../img/demo-image-01.jpg';
-import demoImg02 from '../img/demo-image-02.jpg';
+import WorkItem from '../components/WorkItem';
+
+import demoImg01 from '../img/works/work_02_tumb.jpg';
+import demoImg02 from '../img/works/work_03_tumb.jpg';
+import demoImg03 from '../img/works/work_04_tumb.jpg';
 
 export default class Work extends Component {
   constructor(props) {
@@ -16,54 +19,23 @@ export default class Work extends Component {
     return (
       <section id="work" className="work-section">
         <Container>
-          <div className="">
+          <div className="mb-5">
             <h2 className="section__title">Work</h2>
             <hr className="title__separator ml-0" />
+            <p className="section__subtitle">
+              In this page, you can find a small selection* of projects that I
+              worked on as a Developer, Team Lead or Project Manager.
+              <br />
+              <br />* Unfortunately, some of the projects are copyrighted or
+              covered by a non-disclosure agreement and cannot be made
+              available.
+            </p>
           </div>
 
-          <Row noGutters className="justify-content-center mb-5 mb-lg-0">
-            <Col lg="6">
-              <img className="img-fluid" src={demoImg01} alt="" />
-            </Col>
-
-            <Col lg="6">
-              <div className="bg-black text-center h-100 project">
-                <div className="d-flex h-100">
-                  <div className="project-text mx-4 w-100 my-auto text-center text-lg-left">
-                    <h4 className="">Elit ut sunt id.</h4>
-                    <p className="mb-0">
-                      Sunt dolor commodo minim aliquip id in magna aute id sint
-                      veniam ex dolor cupidatat labore in proident sit in non
-                      occaecat proident id et non enim.
-                    </p>
-                    <hr className="d-none d-lg-block mb-0 ml-0" />
-                  </div>
-                </div>
-              </div>
-            </Col>
-          </Row>
-
-          <Row noGutters className="justify-content-center">
-            <Col lg="6">
-              <img className="img-fluid" src={demoImg02} alt="" />
-            </Col>
-            <Col lg="6" className="order-lg-first">
-              <div className="bg-black text-center h-100 project">
-                <div className="d-flex h-100">
-                  <div className="project-text mx-4 w-100 my-auto text-center text-lg-right">
-                    <h4 className="">Irure aliqua reprehenderit.</h4>
-                    <p className="mb-0 ">
-                      Lorem ipsum veniam exercitation sint nostrud aute elit
-                      tempor irure veniam adipisicing id qui occaecat qui aliqua
-                      eiusmod dolore anim id dolor anim esse amet commodo dolor
-                      in ullamco ad dolore duis ea.
-                    </p>
-                    <hr className="d-none d-lg-block mb-0 mr-0" />
-                  </div>
-                </div>
-              </div>
-            </Col>
-          </Row>
+          <WorkItem src={demoImg01} />
+          <WorkItem src={demoImg02} isFlipped />
+          <WorkItem src={demoImg03} />
+          <WorkItem src={demoImg01} isFlipped />
         </Container>
       </section>
     );

3b7aa4eb8e77bca1ee05a60f250af86854360848
Davi Naizer <davinaizer@gmail.com>
2018-09-14 00:02:26 -0300

    add bulk image load to App. Cache all image files.

--- src/app/index.js
+++ src/app/index.js
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+// IMAGE BULK LOAD
+var cache = {};
+function importAll (r) {
+  r.keys().forEach(key => cache[key] = r(key));
+}
+importAll(require.context('./img/', true, /\.(png|jp(e*)g|svg)$/i));
+// -----------
+
 ReactDOM.render(<App />, document.getElementById('app'));

be344193b6c08efa9b515daa57aa5889615f372b
Davi Naizer <davinaizer@gmail.com>
2018-10-04 23:33:40 -0300

    add index file to Components and Container folders

---
+++ src/app/components/index.js
@@ -0,0 +1,19 @@
+import LinkList from './LinkList';
+import Modal from './Modal';
+import ScrollToTop from './ScrollToTop';
+import SocialIcon from './SocialIcon';
+import TagList from './TagList';
+import WorkInfo from './WorkInfo';
+import WorkItem from './WorkItem';
+import WorkList from './WorkList';
+
+export {
+  LinkList,
+  Modal,
+  ScrollToTop,
+  SocialIcon,
+  TagList,
+  WorkInfo,
+  WorkItem,
+  WorkList,
+};
---
+++ src/app/containers/index.js
@@ -0,0 +1,8 @@
+import About from './About';
+import Cover from './Cover';
+import Contact from './Contact';
+import Navbar from './Navbar';
+import Resume from './Resume';
+import Work from './Work';
+
+export { About, Cover, Contact, Navbar, Resume, Work };

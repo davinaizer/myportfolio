2744cd38d1bbf2dc8ba9d96b366dd14a70188aa6
Davi Naizer <davi.naizer@gmail.com>
2019-10-13 21:12:49 +0100

    move styles to SC

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -1,20 +1,35 @@
-// FSC
-import React from 'react';
 import PropTypes from 'prop-types';
-import './TagList.scss';
+import React from 'react';
+import styled from 'styled-components/macro';
+
+import { colors } from '../styles/theme';
+
+const TagListContainer = styled.ul`
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+`;
+
+const TagListItem = styled.li`
+    background-color: ${colors.jungleGreen};
+    display: inline-block;
+    font-size: 10px;
+    margin-top: 5px;
+    margin-right: 5px;
+    padding: 5px 10px;
+    text-transform: uppercase;
+`;
 
 const TagList = ({ items }) => (
-  <div className="tag-list">
-    <ul>
-      {items.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </ul>
-  </div>
+    <TagListContainer>
+        {items.map((item, index) => (
+            <TagListItem key={index}>{item}</TagListItem>
+        ))}
+    </TagListContainer>
 );
 
 TagList.propTypes = {
-  items: PropTypes.array,
+    items: PropTypes.array
 };
 
 export default TagList;

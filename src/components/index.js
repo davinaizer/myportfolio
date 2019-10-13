60439333e036fed87e4093cf9fd2a168d9526411
Davi Naizer <davi.naizer@gmail.com>
2019-10-13 21:13:29 +0100

    updates WorkInfo import

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
 import LinkList from './LinkList';
 import ScrollToTop from './ScrollToTop';
 import SocialIcon from './SocialIcon';
+import Spinner from './Spinner';
 import TagList from './TagList';
-import WorkInfo from './WorkInfo';
+import WorkInfo from './WorkInfo/WorkInfo';
 import WorkItem from './WorkItem';
 import WorkList from './WorkList';
-import Spinner from './Spinner';
 
 export { LinkList, ScrollToTop, SocialIcon, TagList, WorkInfo, WorkItem, WorkList, Spinner };

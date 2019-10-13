849e6f349f49922212806311d9668174f6a2f986
Davi Naizer <davi.naizer@gmail.com>
2019-10-13 21:12:36 +0100

    move styles to SC

--- src/components/SocialIcon.js
+++ src/components/SocialIcon.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components/macro';
 
-import './SocialIcon.scss';
+const SocialIconContainer = styled.div`
+    display: inline;
+
+    img {
+        height: 80px;
+        margin-right: 30px;
+        margin-top: 30px;
+        width: 80px;
+        transition: all 0.25s;
+
+        &:hover {
+            transform: scale(1.2);
+        }
+    }
+`;
 
 const SocialIcon = ({ icon, href }) => (
-  <div className="social__icon">
-    <a href={href} target="_blank" rel="noopener noreferrer">
-      <img src={icon} alt="" />
-    </a>
-  </div>
+    <SocialIconContainer>
+        <a href={href} target="_blank" rel="noopener noreferrer">
+            <img src={icon} alt="" />
+        </a>
+    </SocialIconContainer>
 );
 
 SocialIcon.propTypes = {
-  icon: PropTypes.string,
-  href: PropTypes.string,
+    icon: PropTypes.string,
+    href: PropTypes.string
 };
 
 export default SocialIcon;

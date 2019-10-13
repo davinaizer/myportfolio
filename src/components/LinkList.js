26ac3f3dfb63da45827ceabee859857e66b904f0
Davi Naizer <davi.naizer@gmail.com>
2019-10-13 21:12:26 +0100

    move styles to SC

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,21 +2,28 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 import PropTypes from 'prop-types';
+import styled from 'styled-components/macro';
 
-import './LinkList.scss';
+/* STYLED COMPONENTS */
+const LinkListContainer = styled.div``;
+
+const LinkButton = styled(Button)`
+    margin-right: 15px;
+    margin-bottom: 15px;
+`;
 
 const LinkList = ({ items }) => (
-  <div className="link-list">
-    {items.map((item, index) => (
-      <a key={index} href={item.url} target="_blank" rel="noopener noreferrer">
-        <Button className="secondary">{item.label}</Button>
-      </a>
-    ))}
-  </div>
+    <LinkListContainer>
+        {items.map((item, index) => (
+            <a key={index} href={item.url} target="_blank" rel="noopener noreferrer">
+                <LinkButton className="secondary">{item.label}</LinkButton>
+            </a>
+        ))}
+    </LinkListContainer>
 );
 
 LinkList.propTypes = {
-  items: PropTypes.array,
+    items: PropTypes.array
 };
 
 export default LinkList;
